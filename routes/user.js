0379c3eec9204760b95e5299b9807a1d3540afab
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-01-31 10:59:24 -0500

    set up a folder for a controllers and built a basic actionm to test

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("hello from node bro!");
-});
+const { sayHi } = require("../controllers/user");
+
+router.get("/", sayHi);
 
 module.exports = router;
