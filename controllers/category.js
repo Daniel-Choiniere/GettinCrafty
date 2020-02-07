12e8789da9ee3f74e453aeb86ba0c67304af09c6
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-07 14:36:24 -0500

    installed lodash and formidable packages

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const { errorHandler } = require("../helpers/dbErrorHAndler");
 
 exports.create = (req, res) => {
   const category = new Category(req.body);
