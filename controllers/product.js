f05f583f2ad100affe36d99d1224144c0eff1ae2
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-02-08 13:50:41 -0500

    built the controller that will ahndle addding a product with an image to the dattabse

--- controllers/product.js
+++ controllers/product.js
@@ -1,1 +1,32 @@
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 const Product = require("../models/product");
+const { errorHandler } = require("../helpers/dbErrorHAndler");
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).josn({
+        error: "Image could not be uploaded"
+      });
+    }
+    let product = new Product(fields);
+
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contnetType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
