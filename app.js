84ff672feda3913ed722e0bbe036c6e68d880ef9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-11 22:58:01 -0400

    fixed bug when setting up braintree

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
-const brainTreeRoutes = require("./routes/braintree");
+const braintreeRoutes = require("./routes/braintree");
 
 const app = express();
 
@@ -38,6 +38,7 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
 
 const port = process.env.PORT || 8000;
 
--- routes/braintree.js
+++ routes/braintree.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { requireSignIn, isAuth } = require("../controllers, auth");
+const { requireSignin, isAuth } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+const { generateToken } = require("../controllers/braintree");
 
-router.get("/braintree/getToken/:userId", requireSignIn, isAuth, generateToken);
-router.param("userId, userById");
+router.get("/braintree/getToken/:userId", requireSignin, isAuth, generateToken);
+router.param("userId", userById);
 
 module.exports = router;
