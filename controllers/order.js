0c2449f979710f3ac3ce41f861f7d4aa96edc8ae
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-13 18:41:10 -0400

    structured our order model, and created the logic for our order controller to create a save an order to the db

--- controllers/order.js
+++ controllers/order.js
@@ -1,3 +1,16 @@
+const { Order, cartItem } = require("../models/order");
+const { errorHandler } = require("../helpers/dbErrorHAndler");
+
 exports.create = (req, res) => {
-  console.log("CREATE ORDER ", req.body);
+  req.body.order.user = req.profile;
+  const order = new Order(req.body.order);
+
+  order.save((error, data) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error)
+      });
+    }
+    res.json(data);
+  });
 };
