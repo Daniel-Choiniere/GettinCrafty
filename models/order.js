3cd5662401b9c9bd17f30e97598f3e1b40a00b82
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-14 11:01:34 -0400

    soooo it turns out we do need name and price in the cart item schema...

--- models/order.js
+++ models/order.js
@@ -5,6 +5,8 @@ const { ObjectId } = mongoose.Schema;
 const CartItemSchema = new mongoose.Schema(
   {
     product: { type: ObjectId, ref: "Product" },
+    name: String,
+    price: Number,
     count: Number
   },
   { timestamps: true }
