58d9b647ff976a98587ed28d4b7ec64a7b3816c8
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-15 19:51:32 -0400

    built the route and controller method for getting a users purchase history

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHAndler");
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -21,21 +22,45 @@ exports.read = (req, res) => {
 };
 
 exports.update = (req, res) => {
-  User.findOneAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true },
-    (err, user) => {
-      if (err) {
+  // console.log('UPDATE USER - req.user', req.user, 'UPDATE DATA', req.body);
+  const { name, password } = req.body;
+
+  User.findOne({ _id: req.profile._id }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found"
+      });
+    }
+    if (!name) {
+      return res.status(400).json({
+        error: "Name is required"
+      });
+    } else {
+      user.name = name;
+    }
+
+    if (password) {
+      if (password.length < 6) {
         return res.status(400).json({
-          error: "You are not authorized to perform this action"
+          error: "Password should be min 6 characters long"
+        });
+      } else {
+        user.password = password;
+      }
+    }
+
+    user.save((err, updatedUser) => {
+      if (err) {
+        console.log("USER UPDATE ERROR", err);
+        return res.status(400).json({
+          error: "User update failed"
         });
       }
-      user.hashed_password = undefined;
-      user.salt = undefined;
-      res.json(user);
-    }
-  );
+      updatedUser.hashed_password = undefined;
+      updatedUser.salt = undefined;
+      res.json(updatedUser);
+    });
+  });
 };
 
 exports.addOrderToUserHistory = (req, res, next) => {
@@ -66,4 +91,18 @@ exports.addOrderToUserHistory = (req, res, next) => {
       next();
     }
   );
+};
+
+exports.purchaseHistory = (req, res) => {
+  Order.find({ user: req.profile._id })
+  .populate("user", "_id name")
+  .sort("-created")
+  .exec((err, orders)) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err)
+      });
+    }
+    res.json(orders);
+  }
 };
