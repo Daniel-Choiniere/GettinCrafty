caa3a8b6bf0c06933714dbf63c6f2112ff010cb7
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-15 20:05:52 -0400

    fixed bug with parenthese in the wrond position in purchaseHistory method

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const { errorHandler } = require("../helpers/dbErrorHAndler");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -95,14 +95,14 @@ exports.addOrderToUserHistory = (req, res, next) => {
 
 exports.purchaseHistory = (req, res) => {
   Order.find({ user: req.profile._id })
-  .populate("user", "_id name")
-  .sort("-created")
-  .exec((err, orders)) => {
-    if (err) {
-      return res.status(400).json({
-        error: errorHandler(err)
-      });
-    }
-    res.json(orders);
-  }
+    .populate("user", "_id name")
+    .sort("-created")
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(orders);
+    });
 };
