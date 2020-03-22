04da1fc16e5e4bbd6305d69c03cb672412b208db
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-22 12:55:32 -0400

    preparing for heroku deployment

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
 const braintreeRoutes = require("./routes/braintree");
 const orderRoutes = require("./routes/order");
+const path = require("path");
 
 const app = express();
 
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.MONGODB_URI || process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -41,6 +42,14 @@ app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../gettincrafty-frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join("../gettincrafty-frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 8000;
 
