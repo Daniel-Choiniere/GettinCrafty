ab4c21610775236227d52bf4d1f74fc9ff795ad5
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-25 21:08:32 -0400

    deploy already...

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const path = require("path");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGODB_URI || process.env.DATABASE, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("DB Connected"));
-
-mongoose.connection.on("error", err => {
-  console.log(`DB Connection error: ${err.message}`);
-});
-
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
+
+  mongoose
+    .connect(process.env.MONGODB_URI || process.env.DATABASE, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("DB Connected"));
+
+  mongoose.connection.on("error", err => {
+    console.log(`DB Connection error: ${err.message}`);
+  });
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
