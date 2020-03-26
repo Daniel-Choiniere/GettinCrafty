026f89cae0266850faf065be386777c6a8d17956
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2020-03-25 20:58:33 -0400

    deploy already...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mongoose = require("mongoose");
 
 mongoose
   .connect(process.env.MONGODB_URI || process.env.DATABASE, {
